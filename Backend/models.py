8b644172bb2ae904109e9a780b39608c4296f56e
Rana Saif <ranasaif9908@gmail.com>
2026-07-12 17:47:52 +0500

    Fixed case sensitivity issues in imports

--- Backend/models.py
+++ Backend/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import func
 from database import Base
 
 class Product(Base):
-    __tablename__ = "zarvix_products"
+    __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
